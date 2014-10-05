aa3c542bf1d651bf767e1d9cafcdc71efab3423c
Diogo Muller <diogo.muller@live.com>
2014-10-05 15:16:00 -0300

    Navigation base.

--- src/gamelib/src/main/java/com/diogomuller/gamelib/core/GameActivity.java
+++ src/gamelib/src/main/java/com/diogomuller/gamelib/core/GameActivity.java
@@ -1,9 +1,41 @@
 package com.diogomuller.gamelib.core;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.ArrayList;
+import java.util.Stack;
 
 /**
  * Created by Diogo on 04/10/2014.
  */
 public class GameActivity extends Activity {
+    private Stack<View> pastViews;
+    private View currentView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+
+        pastViews = new Stack<View>();
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(pastViews.isEmpty()) {
+            super.onBackPressed();
+            return;
+        }
+
+        currentView = pastViews.pop();
+    }
 }
