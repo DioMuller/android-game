5536709280a9aa479c9916e6287c6586f6607045
Diogo Muller <diogo.muller@live.com>
2014-11-09 15:05:17 -0200

    Main/Game activity transition.

--- src/tensecondheroes/src/main/java/com/diogomuller/tensecondheroes/activities/MainActivity.java
+++ src/tensecondheroes/src/main/java/com/diogomuller/tensecondheroes/activities/MainActivity.java
@@ -1,9 +1,11 @@
 package com.diogomuller.tensecondheroes.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.diogomuller.tensecondheroes.R;
 
@@ -13,5 +15,18 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void newgameClick(View v) {
+        Intent intent = new Intent(this, MainGameActivity.class);
+        startActivity(intent);
+    }
+
+    public void loadlevelClick(View v) {
+
+    }
+
+    public void highscoreClick(View v) {
+
     }
 }
