5c8b66762b6ff69608e1c4a0d00d70e6440bb1ce
Diogo Muller <diogo.muller@live.com>
2014-11-16 21:03:53 -0200

    Placeholder classes for entities and scenes.

---
+++ src/tensecondheroes/src/main/java/com/diogomuller/tensecondheroes/entities/ForkliftHero.java
@@ -0,0 +1,7 @@
+package com.diogomuller.tensecondheroes.entities;
+
+/**
+ * Created by Diogo on 16/11/2014.
+ */
+public class ForkliftHero {
+}
---
+++ src/tensecondheroes/src/main/java/com/diogomuller/tensecondheroes/scenes/RunningScene.java
@@ -0,0 +1,7 @@
+package com.diogomuller.tensecondheroes.scenes;
+
+/**
+ * Created by Diogo on 16/11/2014.
+ */
+public class RunningScene {
+}
